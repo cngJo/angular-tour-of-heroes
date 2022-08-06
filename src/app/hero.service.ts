942a023c714586919b69a230337d063e43f46460
Johannes Przymusinski <johannes.przymusinski@jop-software.de>
2022-08-06 11:25:54 +0200

    feat: add dashboard and navigation

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -18,4 +18,11 @@ export class HeroService {
     this.messageService.add("HeroService: fetched heroes");
     return heroes;
   }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: Add proper error handling.
+    const hero = HEROES.find(hero => hero.id === id)!;
+    this.messageService.add(`HeroService: fetches hero id=${id}`);
+    return of(hero);
+  }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -14,8 +14,6 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
 
-  selectedHero?: Hero;
-
   constructor(
     private heroService: HeroService,
     private messageService: MessageService,
@@ -29,10 +27,4 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
-
 }
